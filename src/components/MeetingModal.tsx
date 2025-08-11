96e551f014278f2fc8a41c102dcaf467e4c6e8c6
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-08-11 19:41:21 +0600

    useMeetingActionsHook

--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "./ui/dialog";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import useMeetingActions from "@/hooks/useMeetingsActions";
 
 
 interface MeetingModalProps {
